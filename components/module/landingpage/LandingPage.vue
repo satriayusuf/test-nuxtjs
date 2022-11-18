b114a35e2c2c5e6bdb155d8c77f3892ad88796cb
satriayusuf <satriayusuf21@gmail.com>
2022-11-18 09:20:50 +0700

    [ADD] Landing Page

---
+++ components/module/landingpage/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="pt-28 px-28">
+      <section id="carrousel">
+        <module-landingpage-children-carrousel />
+      </section>
+      <section id="form-simulasi" class="mt-20">
+        <module-landingpage-children-formSimulation />
+      </section>
+      <!-- <section id="list-courier" class="mt-20">
+      <module-landingpage-children-listCourier />
+    </section> -->
+      <section id="mobilitas" class="mt-20">
+        <module-landingpage-children-mobilitas />
+      </section>
+      <section id="banner" class="mt-20">
+        <img
+          src="https://kiriminaja.com/assets/home-2/pesta-pora-new.png"
+          class="w-full h-auto"
+          alt=""
+        />
+      </section>
+      <section id="list-feature" class="mt-20">
+        <module-landingpage-children-listFeature />
+      </section>
+    </div>
+    <div>
+      <section id="cta" class="mt-20">
+        <module-landingpage-children-callToAction />
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {},
+};
+</script>

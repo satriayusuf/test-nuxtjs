201dc108c7efdd98b63503b2dadb475823d073f3
satriayusuf <satriayusuf21@gmail.com>
2022-11-18 09:20:30 +0700

    [ADD] Component Navbar & Footer

---
+++ components/base/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="px-28 py-20">
+    <div class="flex gap-6">
+      <div class="w-4/6">duar</div>
+      <div>duar</div>
+      <div>duar</div>
+    </div>
+  </div>
+</template>
---
+++ components/base/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav id="navbar" class="fixed w-full bg-white">
+    <div class="flex px-28 py-5 justify-between items-center">
+      <div>
+        <img
+          src="https://kiriminaja.com/assets/atrust/svg-logo-primary.svg"
+          class="w-1/4"
+          alt=""
+        />
+      </div>
+      <div class="flex gap-6">
+        <div v-for="menu in listMenu">
+          <router-link :to="menu.path">
+            <p class="font-bold hover:text-blue-900">
+              {{ menu.name }}
+            </p>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      listMenu:[
+        {
+          name: "Beranda",
+          path: "/",
+        },
+        {
+          name: "Integrasi Api",
+          path: "/integrasi-api",
+        },
+        {
+          name: "About",
+          path: "/about",
+        },
+      ]
+    }
+  }
+}
+</script>
